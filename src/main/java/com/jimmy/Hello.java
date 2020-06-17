f59d06112d8c0b6768ab2e92deeb23537fbf7981
Jimmy Yang <yang3gm@gmail.com>
2020-06-17 11:31:30 -0700

    Java and Kotlin variable, data type

--- src/main/java/com/jimmy/Hello.java
+++ src/main/java/com/jimmy/Hello.java
@@ -5,5 +5,22 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19; // age2 becomes a object
+                           // you can have some method to use when you put a dot at the end
+        char c = 'A';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f; //by default, java will use double to storage decimal,
+                              //we need to put "f" at the end of the decimal to force it to use float
+        Float weight2 = 66.5f;
+        double height = 1.7;
+        Double height2 = 1.7;
+        boolean adult = true;
+        boolean enroll = false;
+        Boolean adult2 = true;
+        //Integer, Character, Float, Double, Boolean....這些為了基本資料型態所設計的類別，稱之為 Wrapper class
+        //第一個字大寫
+        String name = "Jimmy"; //String is a (Reference data type), so it must be capital in the first letter
     }
 }
--- src/main/kotlin/com/jimmy/Hello.kt
+++ src/main/kotlin/com/jimmy/Hello.kt
@@ -5,6 +5,19 @@ fun main() {
 //    Human().hello()
     val h = Human()
     h.hello()
+    // Kotlin 沒有 Java 中小寫的資料型態 ex: int, short, double, long, float, char, boolean, byte....
+    // Kotlin 只有物件所定義的資料型態 ex: Int, Short, Double, Long, Float, Char, Boolean, String, Byte....
+    // val (value) ...  value couldn't be changed in the future
+    // var (variable)... value can be changed in the future
+    Byte
+    val age = 19 //val age : Int = 19 --- Kotlin will guess your data type as Int base on the value you gave it
+    // if you change the value of age ex: age = 20 .... it will cause some error
+    var weight = 66.5 //like Java, if you have a decimal... by default, Kotlin will use double data type
+    var weight2 =66.5f
+    val name = "Jimmy"
+    var name2 : String //if you didn't give it a value when you declare a variable...
+                       // you need to declare it's data type
+    name2 = "Jimmy"// then you can give it value later
 }
 
 class Human{
